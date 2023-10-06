10958a69412b26ca008ed909003f13b95d2479e4
Mary <omota4fidelity@gmail.com>
2023-10-06 01:30:58 +0100

    updated

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+  def __init__(self, value = "Hello"):
+    self.value = value
+    pass
+
+  def value(self, val):
+    if (type(val) in (str)):
+      return self.value
+    else: 
+      print("The value must be a string.")
+
+  def is_sentence(self):
+    return self.value.endswith(".")
+  
+  def is_question(self):
+    return self.value.endswith("?")
+  
+  def is_exclamation(self):
+    return self.value.endswith("!")
+  
+  def count_sentences(self):
+    pass
